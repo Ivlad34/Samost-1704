d2a807a8d58bd5445a547ecf2c532f887d4f2283
Ivlad34 <127123191+Ivlad34@users.noreply.github.com>
2024-04-17 21:10:22 +0300

    Add files via upload

---
+++ Sm1704.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void Fier(vector <int> n, int v ) {
+    for (int i = 0; i < n.size(); i++) {
+        n[i] += v;
+    }
+    for (auto i : n) {
+        cout << i << "\n";
+    }
+}
+
+int main()
+{
+    vector <int> i = { 1,2,3,4 };
+    int n = 1;
+    Fier(i, n);
+}
